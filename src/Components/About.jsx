e33cddf032aa8ad2d455cacda6cc7d003f805d15
Mahmud <117104429+MAHMUD1223@users.noreply.github.com>
2023-11-18 23:31:05 +0000

    Update personal information and image

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Web Developer and a Programmer Learning new things everday. I am very passionate about my work.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,11 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Pentesting",
+  "Web accessibility",
+  "Web development",
+  "Programming",
+  "Problem solving",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. I am a self-motivated person who is willing to go above and beyond on any project, and to learn valuable skills on my own time.";
 
 const About = () => {
   return (
